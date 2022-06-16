7b33fa7147b63420c0c5c0d74b79bfa8a69a32ea
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-06-16 14:48:06 -0700

    Stack

---
+++ m_stack.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * m_stack - sets the format of the data to a stack (LIFO)
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void m_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	var.queue = STACK;
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
